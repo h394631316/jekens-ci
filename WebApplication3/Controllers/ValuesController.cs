480d75923e26d312847f0bec329303e09167d5cd
h394631316 <394631316@qq.com>
2019-08-27 23:38:50 +0800

    docker from docker

--- WebApplication3/Controllers/ValuesController.cs
+++ WebApplication3/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplication3.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "hello, jenkins" };
+            return new string[] { "value1", "value2", "hello, jenkins from docker." };
         }
 
         // GET api/values/5
